9607c6b732316a2ea957554ae25f5ec813a3a944
Kohei Imanishi <imanishi@missinglink.co.jp>
2019-04-23 05:44:33 +0900

    add linter and formatter

--- src/src/lightbox/lightbox.component.ts
+++ src/src/lightbox/lightbox.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core'
 @Component({
   selector: 'lightbox',
   templateUrl: './lightbox.component.html',
-  styleUrls: ['./lightbox.component.scss']
+  styleUrls: ['./lightbox.component.scss'],
 })
 export class LightboxComponent {
   @ViewChild('Lightbox') element: ElementRef
